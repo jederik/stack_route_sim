a8f750aeefb44d84127c9fbc26d9ed553c0da4e3
Jann-Frederik Laß <jederik@gmail.com>
2024-04-24 20:37:01 +0200

    remove debug output

--- bin/plot.bash
+++ bin/plot.bash
@@ -5,7 +5,6 @@ PYTHONPATH="src/main:$PYTHONPATH" python -m main "$@" \
 | jq '[.candidates.shortest.transmissions_per_node, .candidates.shortest.efficiency, .candidates.optimised.transmissions_per_node, .candidates.optimised.efficiency]' \
 | jq -r 'map(tostring) | join("\t")' \
 > "$data"
-head "$data"
 gnuplot -p -e "plot \
   '$data' using 1:2 with lines title 'efficiency (normal)', \
   '$data' using 3:4 with lines title 'efficiency (optimised)'" \
