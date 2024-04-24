36bb4189cb55ca12af3239ec3791d193ea4d2bfd
Jann-Frederik Laß <jederik@gmail.com>
2024-04-24 18:21:43 +0200

    adapt script

--- bin/plot.bash
+++ bin/plot.bash
@@ -2,7 +2,7 @@
 
 data=$(mktemp)
 PYTHONPATH="src/main:$PYTHONPATH" python -m main "$@" \
-| jq '[.candidates.random.transmissions_per_node, .candidates.random.efficiency, .candidates.shortest.transmissions_per_node, .candidates.shortest.efficiency]' \
+| jq '[.candidates.shortest.transmissions_per_node, .candidates.shortest.efficiency, .candidates.optimised.transmissions_per_node, .candidates.optimised.efficiency]' \
 | jq -r 'map(tostring) | join("\t")' \
 > "$data"
 head "$data"
